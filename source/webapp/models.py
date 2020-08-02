840ad4ade6bc2202a5c9b9a2945dbc903cdd4d85
Malik <imenov-a@mail.ru>
2020-08-03 04:48:55 +0600

    Добавьте модель Project, применил миграцию ,зарегистрировал в админ панели, Создал несколько проектов в базе. Прикрепил существующие задачи к проектам обновил фикстуры

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,6 +9,8 @@ class Task(models.Model):
     type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Тип',
                              related_name='types')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    project = models.ForeignKey('webapp.Project', null=True, blank=False, related_name='projects',
+                                on_delete=models.PROTECT, verbose_name='Проект')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.summary)
@@ -24,6 +26,18 @@ class Status(models.Model):
 
 class Type(models.Model):
     name = models.CharField(max_length=20, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название проекта')
+    description = models.TextField(max_length=400, null=True, blank=True, verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
     def __str__(self):
         return self.name
