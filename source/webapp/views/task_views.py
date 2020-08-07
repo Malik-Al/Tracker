2b1f5c94e226fd7c70c52286731788ca04b93c98
Malik <imenov-a@mail.ru>
2020-08-08 05:25:30 +0600

    Добавил список задач - поиск по названию и описанию задачи.

--- source/webapp/views/task_views.py
+++ source/webapp/views/task_views.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import  get_object_or_404
 from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
-from webapp.forms import TaskForm
+from webapp.forms import TaskForm, SimpleSearchForm
 from webapp.models import Task
 
 
@@ -12,6 +14,28 @@ class IndexView(ListView):
     context_object_name = 'tasks'
     paginate_by = 1
     paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        form = SimpleSearchForm(self.request.GET)
+        query = None
+        if form.is_valid():
+            query = form.cleaned_data['search']
+        self.form = form
+        self.query = query
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        if self.query:
+            context['query'] = urlencode({'search': self.query})
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.query:
+            queryset = queryset.filter(Q(summary__icontains=self.query) | Q(description__icontains=self.query))
+        return queryset
 
 
 
