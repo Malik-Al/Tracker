de180e5661574c9e38e6c7baf59c35eabd6c3439
Malik <imenov-a@mail.ru>
2020-08-14 06:39:42 +0600

    Добавил страницу регистрации ,подключил базовый шаблон.Сделайте поле email в форме обязательным для заполнения.

---
+++ source/accounts/forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    email = forms.EmailField(label='Email', required=True)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return password_confirm
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('User with this email already exists', code='user_email_exists')
+        except User.DoesNotExist:
+            return email
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'email']
+
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
+from accounts.forms import UserCreationForm
 
 
 def login_view(request):
@@ -23,3 +27,24 @@ def logout_view(request):
     logout(request)
     return redirect('webapp:index')
 
+
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user_1 = User(
+                email=form.cleaned_data['email'],
+                is_active=False)
+            user = form.save()
+            # token = Token.objects.create(user=user)
+            # activation_url = HOST_NAME + reverse('accounts:user_activate') + '?token={}'.format(token)
+
+            # user_1.email_user('Регистрация на сайте localhost',
+            #                   'Для активаций перейдите по ссылке:')
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', context={'form': form})
