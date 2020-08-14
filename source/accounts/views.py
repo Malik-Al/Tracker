d926f33ade32f26fb6cf39c12c98afc428b4b494
Malik <imenov-a@mail.ru>
2020-08-14 06:54:03 +0600

    Добавил в форму валидатор, который проверяет, что хотя бы одно из полей (first_name, last_name) заполнено.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.urls import reverse
-
 from accounts.forms import UserCreationForm
 
 
@@ -30,13 +28,43 @@ def logout_view(request):
 
 
 
+
 def register_view(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+    elif request.method == 'POST':
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
-            user = form.save()
+            user = User(
+                username=form.cleaned_data['username'],
+                first_name= form.cleaned_data['first_name'],
+                last_name = form.cleaned_data['last_name'],
+                email=form.cleaned_data['email']
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
             login(request, user)
             return redirect('webapp:index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', context={'form': form})
+        else:
+            return render(request, 'register.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('webapp:index')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'register.html', context={'form': form})
